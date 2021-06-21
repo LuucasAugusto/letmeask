1dcbc655c2875d339bc2488c444ef51ae89de8fe
Lucas Augusto <lucas.fernandes0522@gmail.com>
2021-06-21 16:15:34 -0300

    Firebase
    Conectando Firebase no React

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button1, Button2 } from "./components/Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button1 text="sou um botão" />
+      <Button2 />
+      <Button1>Sou outro botão</Button1>
+      <Button2 />
     </div>
   );
 }
