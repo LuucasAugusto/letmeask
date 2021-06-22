55bcaeec1dd7b3026bcbe47e506b415c29885202
Lucas Augusto <lucas.fernandes0522@gmail.com>
2021-06-22 00:13:11 -0300

    Continuação dos hooks de autenticação
    Limpando importações que não são mais necessárias, criado 'useAuth' para facilitar a importação do 'useContext' e 'AuthContext'

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,2 +1,8 @@
-import { createContext } from 'react';
+import { useContext } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
+
+export function useAuth() {
+    const value = useContext(AuthContext)
+
+    return value;
+}
