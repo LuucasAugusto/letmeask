00dc6c16d35ba47b4877f9fd4d12c1b7df08d3e4
Lucas Augusto <lucas.fernandes0522@gmail.com>
2021-06-21 23:47:10 -0300

    Contexto de autenticação
    Criando contexto de autenticação, e recuperando o estado de autenticação

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,24 @@
 import { useHistory } from 'react-router-dom';
-import illustrationImg from '../assets/images/illustration.svg'
+import { auth, firebase } from '../services/firebase';
+import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 import '../styles/auth.scss';
 
-import { Button } from '../components/Button'
+import { Button } from '../components/Button';
+import { useContext } from 'react';
+import { AuthContext } from '../App';
 
 export function Home() {
     const history = useHistory();
+    const { user, signInWithGoogle } = useContext(AuthContext)
 
-    function navigateToNewRoom() {
-        history.push('rooms/new')
+    async function handleCreateRoom() {
+        if (!user) {
+            await signInWithGoogle();
+        }
+
+        history.push('/rooms/new');
     }
 
     return (
@@ -23,7 +31,7 @@ export function Home() {
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
-                    <button onClick={navigateToNewRoom} className="create-room">
+                    <button onClick={handleCreateRoom} className="create-room">
                         <img src={googleIconImg} alt="Logo do Google" />
                         Crie sua sala com o Google
                     </button>
